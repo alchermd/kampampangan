25588592683f99b3a869ab373588e6c76c1c5050
alchermd <johnalcherdoloiras@gmail.com>
2018-02-12 23:57:27 +0800

    Add colors to display.

--- app/src/main/java/me/johnalcher/kapampangan/ColorsActivity.java
+++ app/src/main/java/me/johnalcher/kapampangan/ColorsActivity.java
@@ -2,6 +2,9 @@ package me.johnalcher.kapampangan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
+
+        // Populate an ArrayList with the custom Word class.
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Matuling", "Itim", "Black"));
+        words.add(new Word("Berdi", "Luntian", "Green"));
+        words.add(new Word("Malutu", "Pula", "Red"));
+        words.add(new Word("Dilo", "Dilaw", "Yellow"));
+        words.add(new Word("Rosas", "Rosas", "Pink"));
+        words.add(new Word("Maputi", "Puti", "White"));
+        words.add(new Word("Iru", "Asul", "Blue"));
+
+        // Use the custom adapter to store the words list.
+        WordAdapter numbersAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(numbersAdapter);
     }
 }
